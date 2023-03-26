724537e9ba2864dc7bc64ae83440d1f120561983
Ayman Ali <ayman.ali1302@gmail.com>
2023-03-26 18:37:05 +0300

    Initial Commit

---
+++ openrack/openrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  openrackApp.swift
+//  openrack
+//
+//  Created by Ayman Ali on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct openrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
