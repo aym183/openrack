483200bb8a92208fe3330aa528e01b816fa5050a
Ayman Ali <ayman.ali1302@gmail.com>
2023-03-28 13:30:35 +0300

    Added Utilities folder

---
+++ openrack/Application.swift
@@ -0,0 +1,22 @@
+//
+//  Application.swift
+//  openrack
+//
+//  Created by Ayman Ali on 28/03/2023.
+//
+
+import SwiftUI
+
+final class ApplicationUtility {
+    static var rootViewController: UIViewController {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        
+        return root
+    }
+}
--- openrack/Login_ViewModel.swift
+++ openrack/Login_ViewModel.swift
@@ -5,14 +5,18 @@
 //  Created by Ayman Ali on 28/03/2023.
 //
 
-//import SwiftUI
-//import Firebase
-//import GoogleSignIn
-//
-//class SignUpViewModel: ObservableObject {
-//    @Published var isLogin: Bool = false
-//    
-//    func signUpWithGoogl() {
-//        let clien
-//    }
-//}
+import SwiftUI
+import Firebase
+import GoogleSignIn
+
+class SignUpViewModel: ObservableObject {
+    @Published var isLogin: Bool = false
+
+    func signUpWithGoogl() {
+        let clientId = Firebase.app()?.options.clientID else { return }
+        
+        let config = GIDConfiguration(clientID: clientId)
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: <#T##UIViewController#>)
+    }
+}
