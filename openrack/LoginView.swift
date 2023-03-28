3f371f42104a7d7de2a2b67b865497e86c9b697c
Ayman Ali <ayman.ali1302@gmail.com>
2023-03-28 23:35:01 +0300

    Added UI for Sign up

--- openrack/LoginView.swift
+++ openrack/LoginView.swift
@@ -17,90 +17,79 @@ struct LoginView: View {
             VStack{
                 
                 Text("Openrack")
-                    .font(Font.system(size: 70))
-                    .fontWeight(.heavy)
+                    .font(Font.system(size: 70)).fontWeight(.heavy)
                     .foregroundColor(Color("Primary_color"))
-                    .multilineTextAlignment(.center)
                     .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
 
                 Text("Buy, Sell, Discover ✨")
                     .opacity(0.7)
-                    .font(Font.system(size: 20))
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
+                    .font(Font.system(size: 20)).font(.subheadline).fontWeight(.semibold)
                     .padding(.horizontal, 50.0)
+                    .multilineTextAlignment(.center)
                 
                 Text("A peer-to-peer marketplace to buy & sell fashion in MENA.")
-                    .font(Font.system(size: 25))
-                    .padding(.horizontal, 10.0)
-                    .padding(.top, 50.0)
-                    .fontWeight(.semibold)
+                    .font(Font.system(size: 25)).fontWeight(.semibold)
                     .foregroundColor(Color.black)
                     .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10.0).padding(.top, 50.0)
+                    
                 
                 SignInWithAppleButton(
-                    
-                    onRequest: { request in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    },
-                    onCompletion: { result in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    }
+                    onRequest: { request in },
+                    onCompletion: { result in }
                 )
                 .frame(height: 50)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color.black, lineWidth: 2)
+                )
                 .cornerRadius(50)
-                .padding(.top, 70)
-                .padding(.horizontal)
+                .padding(.top, 70).padding(.horizontal)
 
                 
-                Button(action: {
-                    // Action to perform when the button is tapped
-                }) {
+                Button(action: {}) {
                     Image("Facebook_Logo")
-                    Text("Sign in with Facebook")
-                        .font(.title3)
-                        
+                    Text("Sign in with Facebook").font(.title3)
                 }
                 .frame(width: 360, height: 50)
-                .background(Color("Facebook_color"))
-                .foregroundColor(Color.white)
+                .background(Color("Facebook_color")).foregroundColor(Color.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color.black, lineWidth: 2)
+                )
                 .cornerRadius(50)
                 .padding(.horizontal)
                 
-                Button(action: {
-                    DispatchQueue.global().async {
-                        
-                        signupVM.signInWithGoogle()
-                    }
-                }) {
+                Button(action: { DispatchQueue.global().async { signupVM.signInWithGoogle() } })
+                {
                     HStack{
                         Image("Google_Logo")
-                        Text("Sign in with Google")
-                            .font(.title3)
+                        Text("Sign in with Google").font(.title3)
                     }
                     
                 }
                 .frame(width: 360, height: 50)
-                .background(Color("Google_color"))
-                .foregroundColor(Color.white)
+                .background(Color("Google_color")).foregroundColor(Color.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color.black, lineWidth: 2)
+                )
                 .cornerRadius(50)
                 .padding(.horizontal)
                 
-                
-                Button(action: {
-                    showingBottomSheet.toggle()
-                }) {
+                Button(action: { showingBottomSheet.toggle() }) {
                     HStack {
                         Image(systemName: "envelope.fill")
-                        Text("Sign in with Email")
-                            .font(.title3)
+                        Text("Sign in with Email").font(.title3)
                     }
-                    
                 }
                 .frame(width: 360, height: 50)
-                .background(Color("Primary_color"))
-                .foregroundColor(Color.white)
+                .background(Color("Primary_color")).foregroundColor(Color.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color.black, lineWidth: 2)
+                )
                 .cornerRadius(50)
                 .padding(.horizontal)
                 .sheet(isPresented: $showingBottomSheet) {
@@ -109,19 +98,14 @@ struct LoginView: View {
                 
                 Divider().frame(width: 300, height: 3).background(.black).padding(.top, 5).padding(.bottom, 5)
                 
-                Button(action: {
-
-                }) {
+                Button(action: {}) {
                     HStack {
                         Image(systemName: "envelope.fill")
-                        Text("Login with Email")
-                            .font(.title3)
+                        Text("Login with Email").font(.title3)
                     }
-                    
                 }
                 .frame(width: 360, height: 50)
-                .background(.white)
-                .foregroundColor(.black)
+                .background(.white).foregroundColor(.black)
                 .overlay(
                     RoundedRectangle(cornerRadius: 50)
                         .stroke(Color.black, lineWidth: 2)
@@ -130,11 +114,9 @@ struct LoginView: View {
                 .padding(.horizontal)
                 
                 Text("By continuing you agree to our Terms of Service.\nOpenrack services are subject to our Privacy Policy.")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
+                    .font(.footnote).fontWeight(.semibold)
                     .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                    .padding(.top, 20)
+                    .padding(.horizontal).padding(.top, 20)
                     .opacity(0.7)
 
             }
@@ -146,63 +128,59 @@ struct LoginView: View {
 struct BottomSheetView: View {
     @State var usernameText = ""
     @State var passwordText = ""
+    @State private var isOn = false
+    
     var body: some View {
         ZStack {
             Color("Secondary_color").ignoresSafeArea()
-            
-            VStack {
+            VStack (alignment: .leading){
                 
-                Text("Sign Up")
-                    .font(Font.system(size: 30))
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
+                Text("Sign Up").font(Font.system(size: 30)).fontWeight(.heavy).multilineTextAlignment(.trailing).padding(.top, 50)
                 
-                Text("Username")
-                    .font(Font.system(size: 20))
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.leading)
+                
+                Text("Username").font(Font.system(size: 15)).fontWeight(.heavy).padding(.top, 20)
                 
                 
                 TextField("", text: $usernameText)
-//                    .cornerRadius(50)
+                    .frame(width: 360, height: 50).border(Color.black, width: 2)
                     .background(.white)
-                    .frame(width: 360, height: 50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
-                    
+                    .padding(.top, -5)
                 
-                Text("Password")
-                    .font(Font.system(size: 20))
-                    .fontWeight(.semibold)
+                Text("Password").font(Font.system(size: 15)).fontWeight(.heavy).padding(.top, 10)
                 
                 TextField("", text: $passwordText)
-//                    .cornerRadius(50)
+                    .frame(width: 360, height: 50).border(Color.black, width: 2)
                     .background(.white)
-                    .frame(width: 360, height: 50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
+                    .padding(.top, -5)
+                
+                HStack (spacing: 30) {
+                    Toggle(isOn: $isOn) {}.frame(width: 50, height: 50)
+                    Text("Sign up for email to access sales, exclusive drops & more from Openrack")
+                        .font(Font.system(size: 14))
+                        .fontWeight(.medium)
+
+                }
+                
+     
+                Spacer()
                 
                 Button(action: {}) {
                     HStack {
-                        Text("Next")
-                            .font(.title3)
+                        Text("Next").font(.title3)
                     }
-                    
                 }
                 .frame(width: 360, height: 50)
                 .background(Color("Primary_color"))
                 .foregroundColor(.white)
+                .border(Color.black, width: 2)
+                .padding(.bottom)
                 
             }
             .padding(.horizontal)
             .foregroundColor(.black)
-
+            
         }
-        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
