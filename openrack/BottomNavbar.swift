cb9a5389930bfe2fd3c84460a18ec1e23d2e85b4
Ayman Ali <ayman.ali1302@gmail.com>
2023-03-30 20:37:45 +0300

    Added Bottom navbar file for home page

---
+++ openrack/BottomNavbar.swift
@@ -0,0 +1,20 @@
+//
+//  BottomNavbar.swift
+//  openrack
+//
+//  Created by Ayman Ali on 30/03/2023.
+//
+
+import SwiftUI
+
+struct BottomNavbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomNavbar_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomNavbar()
+    }
+}
