d6bf460bb87ba3fc37a1adbbd7b3d10d38854271
Ayman Ali <ayman.ali1302@gmail.com>
2023-04-04 02:18:05 +0300

    Added video-layer

--- openrack/FeedPage.swift
+++ openrack/FeedPage.swift
@@ -6,26 +6,36 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
 
 struct FeedPage: View {
     var body: some View {
-        NavigationStack {
-            VStack{
-                CustomNavbarView()
-                Spacer()
-                ScrollView {
-                   Text("Content")
-                }
-                
-            }
-            .background(Color("Secondary_color"))
-        }
-
-        }
+           VideoPlayerView(videoURL: URL(string: "http://stream.mux.com/XZC6aDM1CxYjFf9C2bxLe7E8JHQ2mj00qjY01NhbNIu2M.m3u8")!)
+               .frame(height: 300)
+       }
 }
 
 struct FeedPage_Previews: PreviewProvider {
     static var previews: some View {
         FeedPage()
+    }
+}
+
+struct VideoPlayerView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = AVPlayerViewController
+
+    var videoURL: URL
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let player = AVPlayer(url: videoURL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        playerViewController.showsPlaybackControls = true
+        return playerViewController
+    }
+
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        // Update view controller
     }
 }
