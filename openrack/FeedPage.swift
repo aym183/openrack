1a3589277783cffa465620da317c0b12ba4bea81
Ayman Ali <ayman.ali1302@gmail.com>
2023-04-06 00:24:35 +0300

    Finalised schedule show popup

--- openrack/FeedPage.swift
+++ openrack/FeedPage.swift
@@ -37,10 +37,47 @@ struct FeedPage: View {
         }
         .background(Color("Secondary_color"))
         .sheet(isPresented: $showingBottomSheet) {
-            Text("Cool Bottom Sheet")
+            StreamBottomSheet(showingBottomSheet: $showingBottomSheet)
                 .presentationDetents([.height(200)])
         }
     }
+    
+
+struct StreamBottomSheet: View {
+    @Binding var showingBottomSheet: Bool
+    var body: some View {
+        ZStack{
+            Color("Primary_color").ignoresSafeArea()
+            VStack {
+                HStack {
+                        Text("Add").font(.title2)
+                        Spacer()
+                    Button(action: { showingBottomSheet.toggle() }) { Image(systemName: "xmark").font(.system(size: 20)) }
+                }
+                .foregroundColor(Color.white)
+                .padding(.horizontal, 30)
+                
+                Divider().frame(width: 400, height: 2).background(.white).padding(.top, 5).padding(.bottom, 15).opacity(0.5)
+                
+                Button(action: { }) {
+                    
+                    HStack {
+                        Image(systemName: "play.fill").font(.system(size: 20)).padding(.trailing, 5)
+                        Text("Schedule a Show").font(.title2)
+                    }
+                    
+                }
+                .frame(width: 360, height: 60)
+                .background(.white).foregroundColor(.black)
+                .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black, lineWidth: 2))
+                .cornerRadius(50)
+                .padding(.vertical)
+            }
+            
+        }
+        
+    }
+}
         
 //        VideoPlayer(player: player)
 //                   .onAppear() {
