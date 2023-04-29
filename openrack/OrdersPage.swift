fa0efee5d4307dc312a964a9f5b02edb8c135ef3
aym183 <ayman.ali1302@gmail.com>
2023-04-29 18:55:20 +0300

    Added orders page

---
+++ openrack/OrdersPage.swift
@@ -0,0 +1,20 @@
+//
+//  OrdersPage.swift
+//  openrack
+//
+//  Created by Ayman Ali on 29/04/2023.
+//
+
+import SwiftUI
+
+struct OrdersPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrdersPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OrdersPage()
+    }
+}
